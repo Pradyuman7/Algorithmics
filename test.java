a5e527de029c9f710c854649d4216c0e37352c52
Pradyuman Dixit <crvasu779@gmail.com>
2018-11-28 09:32:57 +0100

    Create test.java

---
+++ test.java
@@ -0,0 +1,25 @@
+
+public class test {
+
+	public static void main(String[] args) {
+		
+		
+		SievePrimesAlgorithm sp = new SievePrimesAlgorithm();
+		
+		sp.start();
+		
+		for(int i=0;i<20;i++)
+			System.out.println(i+" being prime is "+sp.isPrime(i));
+		
+		NumOccurencesOfCharacters nm = new NumOccurencesOfCharacters();
+		
+		
+		nm.findNumOccurencesOfAllCharacters("hehehe");
+		
+		System.out.println(nm.findNumOccurencesOfCharacter("abcadefgh", 'a' ));
+		
+		
+		
+	}
+	
+}

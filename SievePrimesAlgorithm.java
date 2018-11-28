613a4174ff8980ba0a425061363f933eca7b7e2a
Pradyuman Dixit <crvasu779@gmail.com>
2018-11-28 08:54:43 +0100

    Create SievePrimesAlgorithm.java

---
+++ SievePrimesAlgorithm.java
@@ -0,0 +1,48 @@
+
+public class SievePrimesAlgorithm {
+	
+	public static int N = 1000000;
+	
+	public static boolean prime[] = new boolean[N+1]; 
+	
+	public void start() {
+		
+		for(int i=0;i<N;i++) 
+	        prime[i] = true; 
+		
+		for(int p = 2; p*p <=N; p++) { 
+             
+            if(prime[p] == true) { 
+               
+                for(int i = p*2; i <= N; i += p) 
+                    prime[i] = false; 
+            } 
+        } 
+	}	
+	
+	public boolean isPrime(int n) {
+		
+		
+		if(n<=1)
+			return false;
+		
+		else if(n%2==0)
+			return false;
+		
+		else if(n==2)
+			return true;
+		
+		else {
+			
+			if(prime[n] == true)
+				return true;
+			
+		}
+		
+		
+		return false;
+		
+	}
+	
+
+}

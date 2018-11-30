2c60b7d2f392577e4ab9083f3a58051c1b8e8b37
Pradyuman Dixit <crvasu779@gmail.com>
2018-11-30 14:23:41 +0100

    Create Divisors.java

---
+++ Divisors.java
@@ -0,0 +1,27 @@
+import java.util.ArrayList;
+
+public class Divisors {
+	
+	public ArrayList<Integer> findDivisors(int num) {
+		
+		ArrayList<Integer> div = new ArrayList<Integer>();
+		
+		div.add(1);
+		
+		for(int i=2;i<=num/2;i++) {
+			
+			if(num%i==0)
+				div.add(i);
+			
+		}
+		
+		div.add(num);
+		
+		//for(int i=0;i<div.size();i++)
+		//	System.out.println(div.get(i));
+		
+		return div;
+		
+	}
+
+}

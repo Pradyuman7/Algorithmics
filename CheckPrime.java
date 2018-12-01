a647d775c654d757802ca978ebc56bb1cb3ab909
Pradyuman Dixit <crvasu779@gmail.com>
2018-12-01 10:44:58 +0100

    Create CheckPrime.java

---
+++ CheckPrime.java
@@ -0,0 +1,20 @@
+public class CheckPrime {
+	
+	public boolean checkPrime(long num) {
+		
+		if (num > 2 && num%2 == 0 ) {
+            
+            return false;
+        }
+        int top = (int)Math.sqrt(num) + 1;
+        for(int i = 3; i < top; i+=2){
+            if(num % i == 0){
+                return false;
+            }
+        }
+        return true;
+		
+		
+	}
+
+}

2b7be7243f6b9e8ce35ad78e192fd3530aa08af2
Pradyuman Dixit <crvasu779@gmail.com>
2018-12-05 08:47:22 +0100

    Update GetDivSquareSum.java

--- GetDivSquareSum.java
+++ GetDivSquareSum.java
@@ -2,6 +2,16 @@ public class GetDivSquareSum {
 
   public long sumDivSquare(int num) {
 		
+		
+		if(num==0)
+			return 0;
+		
+		else if(num==1)
+			return 1;
+		
+		else if(num==2)
+			return 5;
+		
 		long sqSum = 1 + num*num;
 		
 		for(int i=2;i<=Math.sqrt(num);i++){
@@ -10,12 +20,15 @@ public class GetDivSquareSum {
 				
 				sqSum+=(i*i);
 				
-				if(num%(num/i)==0) {
+				if(num%(num/i)==0 && num/i!=i) {
 					
 					sqSum+=((num/i)*(num/i));
 					
 				}
+				
 			}
+			
+			
 		}
 		
 		return sqSum;

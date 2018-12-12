be50e244be0f89be8dd67d01779612481caa06b2
Pradyuman Dixit <crvasu779@gmail.com>
2018-12-12 04:31:15 +0100

    Update MaxSubArraySum.java

--- MaxSubArraySum.java
+++ MaxSubArraySum.java
@@ -12,15 +12,16 @@ public class MaxSubArraySum {
 	
 	public int getSum() {
 		
-		int size = arr.length;
-		
-		int max_so_far = arr[0]; 
+	    int size = arr.length;
+	    int max_so_far = arr[0]; 
+	    
 	    int curr_max = arr[0]; 
 	  
 	    for (int i = 1; i < size; i++) { 
 	        curr_max = Math.max(arr[i], curr_max+arr[i]); 
 	        max_so_far = Math.max(max_so_far, curr_max); 
 	    } 
+		
 	    return max_so_far; 
 		
 	}

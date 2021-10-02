020b515b3b46929674adddc97b69faeb552eee36
mKittyD <51280288+mKittyD@users.noreply.github.com>
2021-10-02 20:50:45 +0530

    Update DivisorsTest.java

--- src/test/java/com/pd/algorithms/DivisorsTest.java
+++ src/test/java/com/pd/algorithms/DivisorsTest.java
@@ -5,7 +5,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import java.util.Arrays;
+import java.util.*;
 
 public class DivisorsTest {
     

acf144b9ca37a5bcf16754893b0aa82c5d131d21
Muhammad Usman Tahir <m.usman.tahir.336@gmail.com>
2021-10-03 23:35:54 +0500

    Matrix Multiplication 2D with tests

---
+++ src/main/java/com/pd/algorithms/MatMul2D.java
@@ -0,0 +1,56 @@
+package com.pd.algorithms;
+
+public class Mat_Mul_2D {
+	public int[][] MatrixMult2D(int[][] Mat1, int[][] Mat2) {
+		if(Mat1[0].length == Mat2.length) {
+			int[][] Mat = new int[Mat1.length][Mat2[0].length];
+			for(int i = 0; i<Mat1.length; i++) {
+				for(int j = 0; j<Mat2[0].length; j++) {
+					Mat[i][j] = 0;
+					for(int k = 0; k<Mat2.length; k++) {
+						Mat[i][j] += Mat1[i][k]*Mat2[k][j];
+					}
+				}
+			}
+			return Mat;
+		}else {
+			System.out.println("Matrix cannot be multiplicated due to their uncompatible orders");
+			return null;
+		}
+	}
+	
+	public void ShowMat(int[][] Mat) {
+		if(Mat == null) {
+			System.out.println("Matrix is None");
+		}else {
+			for(int i = 0; i<Mat.length; i++) {
+				for(int j = 0; j<Mat[0].length; j++) {
+					System.out.print(Mat[i][j]);
+					System.out.print(" ");
+				}
+				System.out.println();
+			}
+			System.out.println();
+		}
+	}
+	
+	public boolean EqualMat(int[][] Mat1, int[][] Mat2) {
+		if(Mat1 == null || Mat2 == null) {
+			if(Mat1 != null || Mat2 != null) {
+				return false;				
+			}
+		}
+		if(Mat1[0].length == Mat2[0].length && Mat1.length == Mat2.length) {
+			for(int i = 0; i<Mat1.length; i++) {
+				for(int j = 0; j<Mat1[0].length; j++) {
+					if(Mat1[i][j] != Mat2[i][j]) {
+						return false;
+					}
+				}
+			}
+			return true;
+		}else {
+			return false;
+		}
+	}
+}
---
+++ src/test/java/com/pd/algorithms/MatMul2DTest.java
@@ -0,0 +1,72 @@
+package com.pd.algorithms;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class Mat_Mul_2D_Test {
+	@Test
+	@DisplayName("If it A*I = A")
+	void MatMulTest() {
+		Mat_Mul_2D Obj = new Mat_Mul_2D();
+		int[][] Mat1 = {{1, 2}, 
+						{3, 4},
+						{5, 6}};
+		int[][] Mat2 = {{1, 0}, 
+						{0, 1}};
+		int[][] Mat3 = Obj.MatrixMult2D(Mat1, Mat2);
+		assertTrue(Obj.EqualMat(Mat3, Mat1));
+	}
+	@Test
+	@DisplayName("If I*I = I")
+	void MatMulTest2() {
+		Mat_Mul_2D Obj = new Mat_Mul_2D();
+		int[][] Mat1 = {{1, 0}, 
+						{0, 1}};
+		int[][] Mat3 = Obj.MatrixMult2D(Mat1, Mat1);
+		assertTrue(Obj.EqualMat(Mat3, Mat1));
+	}
+	
+	@Test
+	@DisplayName("If it I*A = A")
+	void MatMulTest3() {
+		Mat_Mul_2D Obj = new Mat_Mul_2D();
+		int[][] Mat1 = {{1, 2}, 
+						{3, 4},
+						{5, 6}};
+		int[][] Mat2 = {{1, 0}, 
+						{0, 1}};
+		int[][] Mat3 = Obj.MatrixMult2D(Mat2, Mat1);
+		assertTrue(Obj.EqualMat(Mat3, Mat1));
+	}
+	@Test
+	@DisplayName("If it A*B = C")
+	void MatMulTest4() {
+		Mat_Mul_2D Obj = new Mat_Mul_2D();
+		int[][] Mat1 = {{1, 2}, 
+						{3, 4},
+						{5, 6}};
+		int[][] Mat2 = {{1, 2, 3},
+						{4, 5, 6}};
+		int[][] Mat3 = Obj.MatrixMult2D(Mat2, Mat1);
+		int[][] Mat4 = {{22, 28},
+						{49, 64}};
+		assertTrue(Obj.EqualMat(Mat3, Mat4));
+	}
+	@Test
+	@DisplayName("If it B*A = D")
+	void MatMulTest5() {
+		Mat_Mul_2D Obj = new Mat_Mul_2D();
+		int[][] Mat1 = {{1, 2}, 
+						{3, 4},
+						{5, 6}};
+		int[][] Mat2 = {{1, 2, 3},
+						{4, 5, 6}};
+		int[][] Mat3 = Obj.MatrixMult2D(Mat1, Mat2);
+		int[][] Mat4 = {{9, 12, 15},
+						{19, 26, 33},
+						{29, 40, 51}};
+		assertTrue(Obj.EqualMat(Mat3, Mat4));
+	}
+}

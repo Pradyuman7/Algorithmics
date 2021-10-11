240527b41c02e15fe064c14fd49ea5f450fbc5c6
Aarzoo Pundir <68347020+aarzoopundir@users.noreply.github.com>
2021-10-11 12:13:35 +0530

    Container with most water

--- src/main/java/com/pd/algorithms/ContainerWithMostWater.java
+++ src/main/java/com/pd/algorithms/ContainerWithMostWater.java
@@ -1,12 +1,12 @@
 package com.pd.algorithms;
 
-//The program should return an integer which corresponds to the maximum area of water that can be contained
+
 public class ContainerWithMostWater {
 	 public static int maxArea(int[] height) {
 	        int area=0;
 	        int start=0;
 	        int end=height.length-1;
-	        //Calculating the max area
+	        
 	        while(start<end){
 	            if(height[start]<height[end]){
 	                area=Math.max(area,height[start]*(end-start));
@@ -19,14 +19,5 @@ public class ContainerWithMostWater {
 	        }
 	        return area;
 	    }
-	 public static void main(String[] args)
-	 {
-	     int a[] = { 1,8,6,2,5,4,8,3,7};
-	     int b[] = { 1,2,1 };
-	  
-	     System.out.println(maxArea(a));
-	     System.out.println(maxArea(b));
-	 }
 }
-//Time Complexity: O(n)
-//Space Complexity: O(1)
+

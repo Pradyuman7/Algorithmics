8094ea3e5ce1265dde3021980962a2e8e3aa4187
DESKTOP-4HC5T4Q\Na Accel One <nadia.ujovich@accelone.com>
2021-10-25 09:20:40 -0300

    fix: Comply last changes with checkstyle

--- src/main/java/com/pd/algorithms/PerfectPowerAlgorithm.java
+++ src/main/java/com/pd/algorithms/PerfectPowerAlgorithm.java
@@ -5,11 +5,15 @@ package com.pd.algorithms;
  */
 public class PerfectPowerAlgorithm {
 
-    public int[] isPerfectPower(int n) {
-        for (int i = 2; ; i++) {
-            int root = (int) Math.round(Math.pow(n, 1.0 / i));
-            if (root < 2) return null;
-            if (Math.pow(root, i) == n) return new int[]{root, i};
-        }
+  public int[] isPerfectPower(int n) {
+    for (int i = 2; ; i++) {
+      int root = (int) Math.round(Math.pow(n, 1.0 / i));
+      if (root < 2) {
+        return null;
+      }
+      if (Math.pow(root, i) == n) {
+        return new int[]{root, i};
+      }
     }
+  }
 }

6516ad8452d73acda810ddeb56900b30e23a0145
Pradyuman7 <crvasu779@gmail.com>
2022-10-01 15:56:38 +0530

    Cleanup

--- src/main/java/com/pd/algorithms/SearchWordInMatrix.java
+++ src/main/java/com/pd/algorithms/SearchWordInMatrix.java
@@ -1,39 +1,37 @@
 package com.pd.algorithms;
 
 public class SearchWordInMatrix {
-
-    public boolean exist(char[][] board, String word) {
-        for (int i = 0; i < board.length; i++) {
-            for (int j = 0; j < board[0].length; j++) {
-                if (dfs(board, word, i, j, 0)) {
-                    return true;
-                }
-            }
+  public boolean exist(char[][] board, String word) {
+    for (int i = 0; i < board.length; i++) {
+      for (int j = 0; j < board[0].length; j++) {
+        if (dfs(board, word, i, j, 0)) {
+          return true;
         }
+      }
+    }
+    return false;
+  }
 
-        return false;
+  private boolean dfs(char[][] board, String word, int i, int j, int index) {
+    if (index == word.length()) {
+      return true;
     }
 
-    private boolean dfs(char[][] board, String word, int i, int j, int index) {
-        if (index == word.length()) {
-            return true;
-        }
-
-        if (i >= board.length || j >= board[0].length || i < 0 || j < 0 || board[i][j] != word.charAt(index)) {
-            return false;
-        }
-
-        char temp = board[i][j];
-        board[i][j] = '*';
-
-        if (dfs(board, word, i + 1, j, index + 1) ||
-                dfs(board, word, i - 1, j, index + 1) ||
-                dfs(board, word, i, j + 1, index + 1) ||
-                dfs(board, word, i, j - 1, index + 1)) {
-            return true;
-        }
-
-        board[i][j] = temp;
-        return false;
+    if (i >= board.length || j >= board[0].length || i < 0 || j < 0 || board[i][j] != word.charAt(index)) {
+      return false;
     }
+
+    char temp = board[i][j];
+    board[i][j] = '*';
+
+    if (dfs(board, word, i + 1, j, index + 1) ||
+            dfs(board, word, i - 1, j, index + 1) ||
+            dfs(board, word, i, j + 1, index + 1) ||
+            dfs(board, word, i, j - 1, index + 1)) {
+      return true;
+    }
+
+    board[i][j] = temp;
+    return false;
+  }
 }
